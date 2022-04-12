cdeabd8de6cdded6a74559c31ca65d4a297e9185
Marcílio Júnior <marciliojr@outlook.com>
2022-04-12 19:34:52 -0300

    adding genre and name search to an ASP.NET Core MVC

--- MvcMovie/MvcMovie/Models/Movie.cs
+++ MvcMovie/MvcMovie/Models/Movie.cs
@@ -1,4 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations; // Necessario para que possamos usar DataAnnotations
+using System.ComponentModel.DataAnnotations.Schema; // DataAnnotations permitem alterar a exibicao de alguns itens
+
 
 namespace MvcMovie.Models
 {
@@ -7,9 +10,12 @@ namespace MvcMovie.Models
         public int Id { get; set; } // Exigido pelo BD, para chave-primaria (PK).
         public string? Title { get; set; }
 
+        [Display(Name = "Release Date")] // O atributo Display especifica o que deve ser exibido no nome de um campo (neste caso, “Release Date” em vez de “ReleaseDate”). 
         [DataType(DataType.Date)] // O usuário não é solicitado a inserir informações de hora no campo de data.
         public DateTime ReleaseDate { get; set; }
         public string? Genre { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")] // Necessário para que o Entity Framework Core possa mapear corretamente o Price para a moeda no banco de dados.
         public decimal Price { get; set; }
     }
 }
